9c0a705df2408028b9360a5bf82644db0c52c68b
Soumya MN <soumya.mn@networksrv.com>
2018-11-12 10:27:58 +0530

    updated the code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
-System.out.println("Hello World Testing!");
-=======
-System.out.println("Hello World sowmyaa!");
->>>>>>> d3959ba8204e608cd5bf572fb671c178d88b6028
+	System.out.println("Hello World sowmyaa!");
+
 	}
 
 }
